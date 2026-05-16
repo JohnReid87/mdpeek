f94d1b39d10b7fbc944e2aaf9770735eca2634e9
John Reid <johne.reid@hotmail.co.uk>
2026-05-16 01:29:26 +0100

    remove smoke tests

--- tests/EzMarkdownViewer.App.Tests/SmokeTests.cs
+++
@@ -1,9 +0,0 @@
-namespace EzMarkdownViewer.App.Tests;
-
-public class SmokeTests
-{
-    [Fact]
-    public void TestHarness_Runs()
-    {
-    }
-}
--- tests/EzMarkdownViewer.Core.Tests/SmokeTests.cs
+++
@@ -1,9 +0,0 @@
-namespace EzMarkdownViewer.Core.Tests;
-
-public class SmokeTests
-{
-    [Fact]
-    public void TestHarness_Runs()
-    {
-    }
-}
